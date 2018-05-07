887e659f85e9dacb30c43706fd8962b5825ea179
GUILHERME TAVEIRA BERSON <05973513173@sefaz.to.gov.br>
2018-05-07 14:39:06 -0300

    Adiconando projeto padrão de android grafico opne gl

---
+++ AG_ProjetoPadrao/app/src/main/java/io/github/bersoncrios/projetopadraohackathon/Jogo.java
@@ -0,0 +1,13 @@
+package io.github.bersoncrios.projetopadraohackathon;
+
+import android.os.Bundle;
+
+import io.github.bersoncrios.projetopadraohackathon.AndGraph.AGActivityGame;
+
+public class Jogo extends AGActivityGame {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ AG_ProjetoPadrao/app/src/main/java/io/github/bersoncrios/projetopadraohackathon/TelaGame.java
@@ -0,0 +1,47 @@
+package io.github.bersoncrios.projetopadraohackathon;
+
+import io.github.bersoncrios.projetopadraohackathon.AndGraph.AGGameManager;
+import io.github.bersoncrios.projetopadraohackathon.AndGraph.AGScene;
+import io.github.bersoncrios.projetopadraohackathon.AndGraph.AGTimer;
+
+public class TelaGame extends AGScene {
+
+    AGTimer tempoTroca = null;
+
+
+    /*******************************************
+     * Name: CAGScene()
+     * Description: Scene construtor
+     * Parameters: CAGameManager
+     * Returns: none
+     *****************************************
+     * @param pManager*/
+    public TelaGame(AGGameManager pManager) {
+        super(pManager);
+    }
+
+    @Override
+    public void init() {
+        tempoTroca = new AGTimer(1000);
+        setSceneBackgroundColor(0,1,1);
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void loop() {
+        tempoTroca.update();
+
+        if (tempoTroca.isTimeEnded()){
+            this.vrGameManager.setCurrentScene(0);
+        }
+    }
+}
